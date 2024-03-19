f2e1595d8152e088c443709e9be790381a11d4ee
sivlvy <vladislavsidenko@ukr.net>
2024-03-19 19:18:15 +0200

    update

---
+++ models/contacts.js
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+import { handleMongooseError } from "../helpers/handleMongooseError.js";
+
+export const createContactSchema = Joi.object({
+	name: Joi.string().required(),
+	email: Joi.string().required(),
+	phone: Joi.string().required(),
+});
+
+export const updateContactSchema = Joi.object({
+	name: Joi.string(),
+	email: Joi.string(),
+	phone: Joi.string(),
+});
+
+export const updateFavoriteSchema = Joi.object({
+	favorite: Joi.boolean().required(),
+});
+const contactSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Set name for contact"],
+		},
+		email: {
+			type: String,
+		},
+		phone: {
+			type: String,
+		},
+		favorite: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleMongooseError);
+
+export const Contact = model("contact", contactSchema);
