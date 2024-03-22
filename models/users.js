ef859ef58aa0759958ebc4f082f6622d8b9c02b7
sivlvy <vladislavsidenko@ukr.net>
2024-03-22 15:33:44 +0200

    upd

---
+++ models/users.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+export const User = new Schema({
+	password: {
+		type: String,
+		required: [true, "Password is required"],
+	},
+	email: {
+		type: String,
+		required: [true, "Email is required"],
+		unique: true,
+	},
+	subscription: {
+		type: String,
+		enum: ["starter", "pro", "business"],
+		default: "starter",
+	},
+	token: {
+		type: String,
+		default: null,
+	},
+});
