8adb98bb7d5b59c3fa2e31c23b5acf38a46f6216
sivlvy <vladislavsidenko@ukr.net>
2024-03-26 15:23:37 +0200

    updated

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -7,21 +7,29 @@ import {
 	createContactSchema,
 	updateContactSchema,
 } from "../../models/contacts.js";
+import { authenticate } from "../../helpers/authenticate.js";
 
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", ctrl.getAllContacts);
+contactsRouter.get("/", authenticate, ctrl.getAllContacts);
 
-contactsRouter.get("/:id", isValidId, ctrl.getOneContact);
+contactsRouter.get("/:id", authenticate, isValidId, ctrl.getOneContact);
 
-contactsRouter.delete("/:id", isValidId, ctrl.deleteContact);
+contactsRouter.delete("/:id", authenticate, isValidId, ctrl.deleteContact);
 
-contactsRouter.post("/", validateBody(createContactSchema), ctrl.createContact);
+contactsRouter.post("/", authenticate, validateBody(createContactSchema), ctrl.createContact);
 
-contactsRouter.put("/:id", isValidId, validateBody(updateContactSchema), ctrl.updateContact);
+contactsRouter.put(
+	"/:id",
+	authenticate,
+	isValidId,
+	validateBody(updateContactSchema),
+	ctrl.updateContact
+);
 
 contactsRouter.patch(
 	"/:id/favorite",
+	authenticate,
 	isValidId,
 	validateBody(updateFavoriteSchema),
 	ctrl.updateStatusContact
